567cea30bc5d2af6bb13d57f1717f63d52cc6a34
Terasumi <tongducthanhnam@gmail.com>
2024-10-06 14:39:40 +0700

    Enhance docker

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,29 +1,32 @@
 import { nextui } from "@nextui-org/theme";
 
-/** @type {import("tailwindcss").Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/react/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       animation: {
-        "meteor-effect": "meteor 15s linear infinite"
+        "meteor-effect": "meteor 15s linear infinite",
+        "text-reveal": "text-reveal 1.5s cubic-bezier(0.77, 0, 0.175, 1) 0.5s",
       },
       keyframes: {
-        meteor: {
-          "0%": { transform: "rotate(215deg) translateX(0)", opacity: 1 },
-          "70%": { opacity: 1 },
+        "text-reveal": {
+          "0%": {
+            transform: "translate(0, 100%)",
+          },
           "100%": {
-            transform: "rotate(215deg) translateX(-1000px)",
-            opacity: 0
-          }
-        }
-      }
-    }
+            transform: "translate(0, 0)",
+          },
+        },
+      },
+    },
   },
   darkMode: "class",
   plugins: [
@@ -35,8 +38,9 @@ module.exports = {
       layout: {}, // common layout tokens (applied to all themes)
       themes: {
         light: {},
-        dark: {}
-      }
-    })
-  ]
+        dark: {},
+      },
+    }),
+  ],
 };
+export default config;
